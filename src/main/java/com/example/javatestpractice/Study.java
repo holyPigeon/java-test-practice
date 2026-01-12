a3383f047c107d5421aee9b040895c3a6d481167
jae woo Jung <awfjol2008@gmail.com>
2026-01-12 15:00:41 +0900

    initialize Java Spring Boot project with Gradle setup and basic structure

---
+++ src/main/java/com/example/javatestpractice/Study.java
@@ -0,0 +1,5 @@
+package com.example.javatestpractice;
+
+public class Study {
+
+}
---
+++ src/test/java/com/example/javatestpractice/StudyTest.java
@@ -0,0 +1,45 @@
+package com.example.javatestpractice;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class StudyTest {
+
+    @Test
+    void create() {
+        Study study = new Study();
+        Assertions.assertNotNull(study);
+        System.out.println("create");
+    }
+
+    @Test
+    void create1() {
+        System.out.println("craete1");
+    }
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("before all");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("after all");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("before each");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("after each");
+    }
+}
